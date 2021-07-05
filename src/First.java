6ed1127197fdd4d9194be36da5aa5807451bbdb6
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-05 13:02:39 +0530

    Initial commit for class

---
+++ src/First.java
@@ -0,0 +1,10 @@
+public class First {
+
+    public static void main(String[] agrs ) {
+
+        System.out.println(" java program");
+        System.out.println("i am prepare java demo class");
+    }
+
+
+}
