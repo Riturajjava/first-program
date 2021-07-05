64b890dad1cfb0b6f0e56ce104f634feef6e7615
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-05 13:13:22 +0530

    added print git

--- src/First.java
+++ src/First.java
@@ -4,6 +4,8 @@ public class First {
 
         System.out.println(" java program");
         System.out.println("i am prepare java demo class");
+        System.out.println("i am git user");
+        System.out.println("i am prepare programing ");
     }
 
 
