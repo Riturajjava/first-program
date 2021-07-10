835db82d253efcf777cfa24d66c1470131076449
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-10 14:25:20 +0530

    java statements while if

--- src/First.java
+++ src/First.java
@@ -9,6 +9,28 @@ public class First {
         // SYNTAX : Datatype variable_name = value
         // you can not declare same name variable again  but can reuse
 
+        String name="Gorav";
+        {
+            System.out.println(name);
+        }
+           name="raj";{
+               System.out.println(name);
+        }
+          String age="28"; {
+        System.out.println("Printedage   " + age); }
+          /* SYNTAX: IF (condition){
+                     code
+                     }
+                     else
+                     {
+                         code
+                     } */
+        int age=28;
+        if (age>30) System.out.println("Person is younger");
+        else {
+            System.out.println("person is kid");
+        }
+
         
 
 
